812c8370f28170af4e99a9ef2cca4b1db41dad72
rosolko.aa <rosolko.aa@pharm-tmn.ru>
2024-10-17 15:03:47 +0500

    1

--- pharm2/SCRIPT.Designer.cs
+++ pharm2/SCRIPT.Designer.cs
@@ -48,6 +48,13 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button4 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // listView2
@@ -60,7 +67,8 @@
             this.columnHeader4,
             this.columnHeader5,
             this.columnHeader1,
-            this.columnHeader2});
+            this.columnHeader2,
+            this.columnHeader3});
             this.listView2.Cursor = System.Windows.Forms.Cursors.Default;
             this.listView2.FullRowSelect = true;
             this.listView2.GridLines = true;
@@ -70,11 +78,11 @@
             this.listView2.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
             listViewGroup1});
             this.listView2.HideSelection = false;
-            this.listView2.Location = new System.Drawing.Point(12, 12);
+            this.listView2.Location = new System.Drawing.Point(1, 0);
             this.listView2.MultiSelect = false;
             this.listView2.Name = "listView2";
             this.listView2.ShowGroups = false;
-            this.listView2.Size = new System.Drawing.Size(324, 636);
+            this.listView2.Size = new System.Drawing.Size(335, 648);
             this.listView2.TabIndex = 71;
             this.listView2.UseCompatibleStateImageBehavior = false;
             this.listView2.View = System.Windows.Forms.View.Details;
@@ -88,12 +96,12 @@
             // columnHeader5
             // 
             this.columnHeader5.Tag = "321";
-            this.columnHeader5.Text = "Парус";
+            this.columnHeader5.Text = "IP";
             this.columnHeader5.Width = 113;
             // 
             // columnHeader1
             // 
-            this.columnHeader1.Text = "IP";
+            this.columnHeader1.Text = "Парус";
             this.columnHeader1.Width = 100;
             // 
             // columnHeader2
@@ -123,11 +131,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(342, 59);
+            this.label1.Location = new System.Drawing.Point(339, 59);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.Size = new System.Drawing.Size(80, 13);
             this.label1.TabIndex = 81;
-            this.label1.Text = "Файл отчета";
+            this.label1.Text = "Файл скрипта";
             // 
             // textBox3
             // 
@@ -149,7 +157,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(342, 114);
+            this.label3.Location = new System.Drawing.Point(527, 166);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 78;
@@ -158,7 +166,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(342, 88);
+            this.label2.Location = new System.Drawing.Point(527, 140);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(33, 13);
             this.label2.TabIndex = 77;
@@ -166,35 +174,36 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(398, 111);
+            this.textBox2.Location = new System.Drawing.Point(583, 163);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(210, 20);
+            this.textBox2.Size = new System.Drawing.Size(98, 20);
             this.textBox2.TabIndex = 76;
             this.textBox2.Text = "100-ecgt[jd";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(398, 85);
+            this.textBox1.Location = new System.Drawing.Point(583, 137);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(210, 20);
+            this.textBox1.Size = new System.Drawing.Size(98, 20);
             this.textBox1.TabIndex = 75;
             this.textBox1.Text = "sysdba";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(614, 79);
+            this.button1.Location = new System.Drawing.Point(345, 137);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(80, 52);
             this.button1.TabIndex = 74;
             this.button1.Text = "Поехали";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(342, 257);
+            this.richTextBox1.Location = new System.Drawing.Point(342, 137);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(352, 391);
+            this.richTextBox1.Size = new System.Drawing.Size(352, 511);
             this.richTextBox1.TabIndex = 82;
             this.richTextBox1.Text = "";
             // 
@@ -204,11 +213,11 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(700, 12);
+            this.textBox4.Location = new System.Drawing.Point(700, 257);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox4.Size = new System.Drawing.Size(401, 636);
+            this.textBox4.Size = new System.Drawing.Size(401, 391);
             this.textBox4.TabIndex = 83;
             // 
             // backgroundWorker1
@@ -216,11 +225,74 @@
             this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork_1);
             this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted_1);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(342, 85);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(352, 46);
+            this.button4.TabIndex = 84;
+            this.button4.Text = "Поехали";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(700, 47);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(401, 204);
+            this.treeView1.TabIndex = 85;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(788, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 87;
+            this.label4.Text = "Успешно";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(847, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(10, 13);
+            this.label5.TabIndex = 88;
+            this.label5.Text = "-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(941, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 89;
+            this.label6.Text = "Ошибка";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(994, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(10, 13);
+            this.label7.TabIndex = 90;
+            this.label7.Text = "-";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "pass";
+            // 
             // SCRIPT
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1113, 657);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label1);
@@ -236,6 +308,7 @@
             this.Controls.Add(this.button2);
             this.Name = "SCRIPT";
             this.Text = "SCRIPT";
+            this.Load += new System.EventHandler(this.SCRIPT_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -262,5 +335,12 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.TextBox textBox4;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
     }
 }
--- pharm2/SCRIPT.cs
+++ pharm2/SCRIPT.cs
@@ -8,7 +8,9 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -26,7 +28,7 @@ namespace pharm2
             PATH = Settings1.Default.PATH;
             NpgsqlConnection con = new NpgsqlConnection(PATH);
             con.Open();
-            string sql = "SELECT ip_server as ip,kod_parus as par,alias as al FROM \"Apteka\" ORDER BY par;";
+            string sql = "SELECT ap.ip_server as ip,ap.kod_parus as par,ap.alias as al,pp.word as pas FROM \"Apteka\" ap,\"pass\" pp WHERE pp.id = ap.pass_id ORDER BY par;";
             NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, con);
             ds.Reset();
             da.Fill(ds);
@@ -39,7 +41,7 @@ namespace pharm2
             foreach (DataRow row in dt.Rows)
             {
 
-                string[] rt = { "", row["ip"].ToString(), row["par"].ToString(), row["al"].ToString() };
+                string[] rt = { "", row["ip"].ToString(), row["par"].ToString(), row["al"].ToString(), row["pas"].ToString() };
                 var listViewItem = new ListViewItem(rt);
                 listView2.Items.Add(listViewItem);
             }
@@ -212,6 +214,250 @@ namespace pharm2
         {
             string b = (string)e.Result;
             //MessageBox.Show(b);
+        }
+
+        public void FDB( string dms,string user,string pass,string pyt)
+        {
+            string[] words = dms.Split(new char[] { ':' });
+            int check = pingip(words[0]);
+            //this.richTextBox1.Invoke((MethodInvoker)delegate {
+                // Running on the UI thread
+            //    this.richTextBox1.AppendText(dms +" - "+ check.ToString() + Environment.NewLine);
+            //});
+            
+            if (check == 1)
+            {
+                string[] kodpass = dms.Split(new char[] { '{' });
+                string[] kod = kodpass[1].Split(new char[] { '}' });
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                //startInfo.CreateNoWindow = true;
+                string cd = "\"C:\\Program Files (x86)\\Firebird\\Firebird_2_5\\bin\\isql.exe\"";
+                startInfo.Arguments = "/C "+cd+" -user "+ user +" -password "+ pass +" "+ kodpass[0] + " -ch WIN1251 -m -i "+ pyt;
+                process.StartInfo = startInfo;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.Start();
+                //* Read the output (or the error)
+                string output = process.StandardOutput.ReadToEnd();
+                //Console.WriteLine(output);
+                string err = process.StandardError.ReadToEnd();
+                //Console.WriteLine(err);
+                process.WaitForExit();
+
+
+
+                if(output == "")
+                {
+                    
+                        this.treeView1.Invoke((MethodInvoker)delegate
+                        {
+                            // Running on the UI thread
+                            this.treeView1.Nodes[0].Nodes.Add(new TreeNode(dms));
+                        });
+                        this.label5.Invoke((MethodInvoker)delegate
+                        {
+                            yes += 1;
+                            this.label5.Text = yes.ToString();
+                        });
+
+                        this.listView2.Invoke((MethodInvoker)delegate
+                        {
+                            ListViewItem item1 = listView2.FindItemWithText(kod[0], true, 0);
+                            if (item1 != null)
+                            {
+                                this.listView2.Items[item1.Index].BackColor = Color.Green;
+                            }
+                        });
+
+                }
+                else
+                {
+                    
+                        this.richTextBox1.Invoke((MethodInvoker)delegate {
+                            // Running on the UI thread
+                            this.richTextBox1.AppendText(dms + " - " + output + Environment.NewLine);
+                        });
+                        this.treeView1.Invoke((MethodInvoker)delegate {
+                            // Running on the UI thread
+                            this.treeView1.Nodes[1].Nodes.Add(new TreeNode(dms));
+                        });
+                        this.label7.Invoke((MethodInvoker)delegate {
+                            no += 1;
+                            this.label7.Text = no.ToString();
+                        });
+                        this.listView2.Invoke((MethodInvoker)delegate
+                        {
+                            ListViewItem item1 = listView2.FindItemWithText(kod[0], true, 0);
+                            if (item1 != null)
+                            {
+                                this.listView2.Items[item1.Index].BackColor = Color.Red;
+                            }
+                        });
+                    
+                }  
+                
+            }
+            else
+            {
+                this.treeView1.Invoke((MethodInvoker)delegate {
+                    // Running on the UI thread
+                    this.treeView1.Nodes[1].Nodes.Add(new TreeNode(dms));
+                });
+                this.label7.Invoke((MethodInvoker)delegate {
+                    no += 1;
+                    this.label7.Text = no.ToString();
+                });
+            }
+
+        }
+ 
+        public bool chekip(string ip)
+        {
+            if (string.IsNullOrEmpty(ip))
+            {
+                return false;
+
+            }
+            else
+            {
+                string pattern = @"\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b";
+                if (Regex.IsMatch(ip, pattern))
+                {
+                    return true;
+                }
+                else
+                {
+                    this.richTextBox1.Invoke((MethodInvoker)delegate {
+                        // Running on the UI thread
+                        this.richTextBox1.AppendText("Не верный IP!!! - " + ip + Environment.NewLine);
+                    });
+                    
+                    return false;
+                }
+            }
+
+        }
+        PingReply rep;
+        int pingip(String pp)
+        {
+            int st = 0;
+            String mac = "";
+            if (string.IsNullOrEmpty(pp))
+            {
+                richTextBox1.AppendText("Нет IP!!! - " + pp + Environment.NewLine);
+            }
+            else
+            {
+                if (chekip(pp))
+                {
+                    mac = pp;
+
+                }
+                else { richTextBox1.AppendText("Ошибка IP!!! - " + pp + Environment.NewLine); }
+            }
+
+            Ping pingreq = new Ping();
+
+            try
+            {
+               // richTextBox1.Clear();
+
+                Ping ping =
+            new Ping();
+                PingReply pingReply = ping.Send(mac);
+               // richTextBox1.Text += " " + pingReply.RoundtripTime; //время ответа
+               // richTextBox1.Text += " " + pingReply.Status;        //статус
+               // richTextBox1.Text += " " + pingReply.Address;       //IP
+
+                rep = pingreq.Send(mac);
+                // richTextBox1.Text += "  Reply From {" + rep.Address.ToString() + "} : time={" + rep.RoundtripTime + "} TTL={" + rep.Options.Ttl + "}" + "\r\n";
+                this.richTextBox1.Invoke((MethodInvoker)delegate {
+                    // Running on the UI thread
+                    this.richTextBox1.AppendText("ping - " + mac + Environment.NewLine);
+                    st =1;
+                });
+                
+
+            }
+            catch (Exception) // тут перехватывайте нужный тип исключения
+            {
+                st = 0;
+            }
+            finally
+            {
+                
+            }
+            return st;
+        }
+        int yes = 0;
+        int no = 0;
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+            treeView1.Nodes.Clear();
+            richTextBox1.Clear();
+            list.Clear();
+            for (int i = 0; i < listView2.Items.Count; i++)
+            {
+
+                if (listView2.Items[i].Checked)
+                {
+
+                    list.Add(this.listView2.Items[i].SubItems[1].Text.ToString() + ':' + listView2.Items[i].SubItems[3].Text.ToString() + '{' + listView2.Items[i].SubItems[2].Text.ToString()+'}'+ listView2.Items[i].SubItems[4].Text.ToString());
+                }
+
+
+
+            }
+            TreeNode us = new TreeNode("Успешно");
+            TreeNode oo = new TreeNode("Ошибка");
+            treeView1.Nodes.Add(us);
+            treeView1.Nodes.Add(oo);
+
+
+            int ind = 1;
+            foreach (var item in list)
+            {
+                //treeView1.Nodes[0].Nodes.Add(new TreeNode(item));
+                string[] pp = item.Split(new char[] { '}' });
+                List<Thread> ThrArr = new List<Thread>();
+                Thread devthread = new Thread(() => FDB(item,"SYSDBA", pp[1], textBox3.Text));
+                devthread.Name = "thr" + ind.ToString();
+                ThrArr.Add(devthread);
+                devthread.Start();
+                richTextBox1.AppendText("Поток зап - " + devthread.Name + Environment.NewLine);
+                
+                ind++;
+            }
+            /*
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (await Task.Run(() => ExecuteBatchScript("database=" + list[i] + ";user=" + textBox1.Text + ";password=" + textBox2.Text, textBox3.Text, listOtvet)))
+                {
+                    listOtvet.Add("Успешно!" + list[i]);
+                }
+                else { listOtvet.Add("Ошибка!" + list[i]); }
+
+            }
+
+            richTextBox1.Clear();
+            for (int i = 0; i < listOtvet.Count; i++)
+            {
+                richTextBox1.Text += listOtvet[i] + Environment.NewLine;
+            }*/
+
+        }
+
+     
+
+        private void SCRIPT_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
